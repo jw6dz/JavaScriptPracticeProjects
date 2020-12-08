447901b058d6393ba092e51d04a38e7e55b5ba75
Josh <josh.wang@circulation.com>
2020-12-08 00:14:01 -0800

    feat: add Draggable functionality to QuoteCard

--- src/components/TronaldQuotes/QuoteCard.js
+++ src/components/TronaldQuotes/QuoteCard.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useRef, useState }from 'react'
+import Draggable from 'react-draggable'
 
-const QuoteCard = ({ date, quoteId, message, tags}) =>
-  <div key={quoteId} className="QuoteCard">
-    <div className="QuoteCard__Message">
-      <span className="QuoteCard__QuotationMark">"</span> {message} <span className="QuoteCard__QuotationMark">"</span>
-    </div>
-    <div>
-      
-    </div>
-    <div className="QuoteCard__Meta">
-      {!!date && <div>Words Uttered On: {date}</div>}
-      {tags.length > 0 && <div>Tags: {tags.join(', ')}</div>}
-    </div>
-  </div>
+const QuoteCard = ({ date, getNextQuote, getPreviousQuote, quoteId, message, tags}) => {
+  const [x, setX] = useState(0)
+  const quoteCardRef = useRef(null)
+
+  const resetPos = (e, data) => {
+    if (data.x > 50) getPreviousQuote()
+    else if (data.x < -50) { 
+      getNextQuote() // TO-DO: fix bug where this changes current QuoteCard object when called from here
+    }
+    setX(0)
+  }
+
+  return (
+    <Draggable 
+      axis="x"
+      defaultPosition={{x: 0, y: 0}}
+      nodeRef={quoteCardRef}
+      onStop={(e, data) => resetPos(e, data)}
+      position={{x, y: 0}}
+    >
+      <div key={quoteId} className="QuoteCard" ref={quoteCardRef}>
+        <div className="QuoteCard__Message">
+          <span className="QuoteCard__QuotationMark">"</span> {message} <span className="QuoteCard__QuotationMark">"</span>
+        </div>
+        <div>
+          
+        </div>
+        <div className="QuoteCard__Meta">
+          {!!date && <div>Words Uttered On: {date}</div>}
+          {tags.length > 0 && <div>Tags: {tags.join(', ')}</div>}
+        </div>
+      </div>
+    </Draggable>
+  )
+}
 
 export default QuoteCard
--- src/components/TronaldQuotes/QuotesContainer.js
+++ src/components/TronaldQuotes/QuotesContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import axios from 'axios'
 import { baseApiUrl } from './QuotesHome'
 import QuoteCard from './QuoteCard'
@@ -8,18 +8,9 @@ const QuotesContainer = () => {
   const [quotes, setQuotes] = useState([])
   const [activeQuoteIndex, setActiveQuoteIndex] = useState(0)
   
-  const nextQuoteAvailable = activeQuoteIndex < quotes.length - 1
-  const previousQuoteAvailable = activeQuoteIndex > 0 && !!quotes
-
-  const fetchRandomQuote = async () => {
-    try {
-      const { data } = await axios.get(`${baseApiUrl}/random/quote`)
-      setQuotes([...quotes, <QuoteCard date={moment(data.appeared_at).format("MMM Do, YYYY")} quoteId={data.quote_id} message={data.value} tags={data.tags} />])
-    } catch (err) {
-      console.log('Random Quote fetching failed b/c: ', err)
-    }
-  }
-  
+  const nextQuoteAvailable = useMemo(() => activeQuoteIndex < quotes.length - 1, [quotes, activeQuoteIndex])
+  const previousQuoteAvailable = useMemo(() => activeQuoteIndex > 0 && !!quotes, [quotes, activeQuoteIndex])
+ 
   const getNextQuote = () => {
     if (nextQuoteAvailable) setActiveQuoteIndex(activeQuoteIndex + 1)
     else {
@@ -29,9 +20,29 @@ const QuotesContainer = () => {
 
   const getPreviousQuote = () => {
     if (previousQuoteAvailable) setActiveQuoteIndex(activeQuoteIndex - 1)
+    else return null
+  }
+
+  const fetchRandomQuote = async () => {
+    try {
+      console.log('fetching random quote', quotes)
+      const { data } = await axios.get(`${baseApiUrl}/random/quote`)
+      setQuotes([...quotes, 
+        <QuoteCard 
+          date={moment(data.appeared_at).format("MMM Do, YYYY")} 
+          getNextQuote={getNextQuote}
+          getPreviousQuote={getPreviousQuote}
+          quoteId={data.quote_id} 
+          message={data.value} 
+          tags={data.tags} />
+        ])
+    } catch (err) {
+      console.log('Random Quote fetching failed b/c: ', err)
+    }
   }
 
   useEffect(() => {
+    console.log('quotes cahnged', quotes)
     setActiveQuoteIndex(quotes.length - 1)
   }, [quotes])
 
@@ -59,7 +70,6 @@ const QuotesContainer = () => {
           More Nonsense
         </button>
       </div>
-
     </div>
   )
 }
